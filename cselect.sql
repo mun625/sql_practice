b0a4f6fbdc4bc51ec45f74edf2c43f1532066c44
mun625 <mun625@gmail.com>
2016-06-03 13:26:46 +0200

    complete problem c query and result

--- cselect.sql
+++ cselect.sql
@@ -1,7 +1,9 @@
 .open reuters.db
 
 .output 'part_c.txt'
-select count(term) from Frequency where docid = "10398_txt_earn" and count = 1
-union
-select count(term) from Frequency where docid = "925_txt_trade" and count = 1
+select count(*) from(
+	select term from Frequency where docid = "10398_txt_earn" and count = 1
+	union
+	select term from Frequency where docid = "925_txt_trade" and count = 1
+	);
 .output stdout
